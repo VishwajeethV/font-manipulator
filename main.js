e95cfbc561d8a2def0e5073adc25afddbc1832b1
VishwajeethV <spacexblue80@gmail.com>
2021-02-16 20:05:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 function setup() {
     video = createCapture(VIDEO);
-    video.size(400, 400);  
+    video.size(400, 400); 
+    video.position(); 
     canvas = createCanvas(400, 400);
     canvas.position(500,100);
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',getposes);
+}
+
+function modelLoaded() {
+    console.log("posenet is intialized");
+}
+
+
+function gotposes(results) {
+    if(results.length > 0) {
+        console.log(results);
+        leftwristx=results[0].pose.leftWrist.x;
+        rightwristx=results[0].pose.rightWrist.x;
+        difference=floor(leftwristx-rightwristx);
+        console.log(difference);
+    }
 }
